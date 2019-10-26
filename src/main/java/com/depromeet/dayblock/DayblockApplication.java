69d5e3258ee93851a46ebdab3c533d65ca2a376d
Jisu Son <liloyek@gmail.com>
2019-10-26 14:59:50 +0900

    first commit

---
+++ src/main/java/com/depromeet/dayblock/DayblockApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.dayblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayblockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayblockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depromeet/dayblock/DayblockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.dayblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayblockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
